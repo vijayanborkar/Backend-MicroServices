173c5757b5783f31ade4b0b6da0fbe5d1e1fc31d
vijayanborkar <vijayanborkar27@gmail.com>
2024-11-22 22:15:25 +0530

    feat: two apis for concert-planning and trip-planning

---
+++ trip_planner/index.js
@@ -0,0 +1,29 @@
+const { response } = require("express");
+const axiosInstance = require("./lib/axios");
+require("dotenv").config();
+
+axiosInstance
+  .get("/health")
+  .then((response) => console.log(response.data))
+  .catch((error) => console.log("Error fetching the axios health", error));
+
+const getFlight = async (origin, destination) => {
+  try {
+    const response = await axiosInstance.get("/flights/search", {
+      params: {
+        origin: origin,
+        destination: destination,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getFlight("bengaluru", "dehradun")
+  .then((flights) => console.log("Flights data", flights))
+  .catch((error) => {
+    console.log(error);
+  });
